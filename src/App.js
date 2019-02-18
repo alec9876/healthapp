e9b96ab1f1d0daca13fa39b63dc3469c55aa32da
alec9876 <alecschram@hotmail.com>
2019-02-18 15:30:50 -0500

    being to add browser routers/added new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
+
+
+// Components
+// import FoodLog from './FoodLog.js';
+// import Exercise from './Exercise';
+// import Eateries from './Eateries';
+// import Home from './Home';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-        <header className="">
-
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          {/* <Route exact path="/" component={Home} />
+          <Route path="/foodlog" component={FoodLog} />
+          <Route path="/exercise" component={Exercise}/>
+          <Route path="/eateries" component={Eateries}/>  */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+
+const Header = props =>
+  <header>
+    <div className='header-container'>
+        <h1 className="header-h1"><a href="#">HealthWatch</a></h1>
+        <nav>
+            <ul className="navigation">
+                <li><a href="#">FoodLog</a></li>
+                <li><a href="#">Exercise</a></li>
+                <li><a href="#">Eateries</a></li>
+            </ul>
+        </nav>
+    </div>
+        
+  </header>;
+
+  export default Header;
