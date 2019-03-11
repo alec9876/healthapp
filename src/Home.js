c66530a81a04973b8f6eeeb46d608fd2467e6f0d
alec9876 <alecschram@hotmail.com>
2019-03-11 16:59:53 -0400

    Combined totals of Calories Consumed and Calories Expended to get Total

--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,35 @@ import React, { Component } from 'react';
 class Home extends Component {
     state={
         calories: localStorage.getItem("calorieCount") ? parseInt(localStorage.getItem("calorieCount")) : 0,
-        exercises: parseInt(localStorage.getItem("calorieBurn")) 
+        exercises: localStorage.getItem("calorieBurn") ? parseInt(localStorage.getItem("calorieBurn")) : 0
       }
 
 
       clearCalories = () => {
-        this.setState({calories: localStorage.clear() ? 0 : 0 });
+        this.setState({
+            calories: localStorage.clear() ? 0 : 0,
+            exercises: localStorage.clear() ? 0 : 0 
+        });
+        
       }
+
+     
     
     render() {
     return (
     <div>
         <h1>Calories Consumed</h1>
-            {this.state.calories} 
+        {this.state.calories}
         <h1>Calories Expended</h1>
-            {this.state.exercises}
+        {this.state.exercises}
+        <h1>Total Calories</h1>    
+        {this.state.calories - this.state.exercises}
         <div>
             <button onClick={() => this.clearCalories()}>
                 Clear All Calories
             </button>   
         </div>
+        
      </div>
     )
     }
